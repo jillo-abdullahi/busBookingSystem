689aaf95e5058ae7ab9a2700a5f478bb8156f82b
Jillo Abdullahi <jillo.abdulla@gmail.com>
2017-05-13 12:01:27 +0300

    more changes to the index page

--- index.php
+++ index.php
@@ -1,20 +1,18 @@
 <!DOCTYPE html>
 <html>
-<head>
-	
-
+  <head>
+    <title>Zain Services</title>
 	<meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="Bus reservation">
     <meta name="author" content="Jillo Abdullahi">
-    <!--<link rel="shortcut icon" href="../../assets/ico/favicon.ico">-->
-
-    <title>Zain Services</title>
-
-    <!-- Bootstrap core CSS -->
-   <link rel='stylesheet prefetch' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css'>
    
+    <!-- Bootstrap-->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
    <!--Google font-->
    <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css">
    <!--Font-awesome-->
@@ -41,24 +39,24 @@
 
 
 
-</head>
+  </head>
 <body>
 
 <!--Navigation bar and the various menu items-->
 
-<div class="navbar navbar-inverse navbar-static-top">
-	<div class="container">
-		<a href="index.php" class="navbar-brand">Zayn Services</a>
-		<button class="navbar-toggle" data-toggle="collapse" data-target =".navHeaderCollapse">
-			
-			<span class ="icon-bar"></span>
-			<span class ="icon-bar"></span>
-			<span class ="icon-bar"></span>
+<nav class="navbar navbar-inverse navbar-fixed-top" id="navbar">
+	<div class="container-fluid">
+	  <div class="navbar-header">
+		<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navHeaderCollapse">
+		  <span class="icon-bar"></span>
+		  <span class="icon-bar"></span>
+		  <span class="icon-bar"></span>	
 		</button>
-		<div class="collapse navbar-collapse navHeaderCollapse">
-			
+		<a class="navbar-brand" href="index.php">Zayn Services</a>
+	  </div>
+	
+	  <div class="collapse navbar-collapse" id ="navHeaderCollapse">
 			<ul class="nav navbar-nav navbar-right">
-				
 				<li class="active"><a href="#">Home</a></li>
 				<li><a href="aboutUs.php">About Us</a></li>
 				<li><a href="#contact" data-toggle="modal">Contact Us</a></li>
@@ -67,74 +65,68 @@
 			</ul>
 		</div>
 	</div>
+</nav>
 
-</div>
 
 
-<!--Jumbotron -->
- <div class="container" >
-    <div class="jumbotron">
-      <div class="container">
-	    <div class="row">
-		  <div class="col-lg-12">
-            <h2>Proudly serving you.<br>Because the journey is just as important as the destination.</h2>
-		  </div>
-		 </div>
+
+<!--Intro-header-->
+<div class="intro-header">
+  <div class="container">
+      <div class="row">
+		  <div class="col-sm-12">
+		    <div class="intro-message">
+              <h1>Proudly serving you.<br>Because the journey is just as important as the destination.</h1>
+			</div>
+		  </div> 
       </div>
-    </div>
-    </div>
+   </div>
+</div>
 
 <!--travel specifications form-->
 
 <div class="container">
-<div class="jumbotron" id="middlejumbotron">
+  <div class="jumbotron">
+    <div class="col-lg-6"></div>
+		<form class="form-horizontal col-lg-6" id="booking_details" action="seatsPage.php" method="POST" onsubmit="return validateForm()">
+			<div class="form-group">
+				<label for="source" class="col-lg-4 control-label" >From:</label>
+					<div class="col-lg-8">
+						<select class="form-control" id="from" name="from">
+							<option value="nairobi">Nairobi</option>
+							<option value="isiolo">Isiolo</option>
+							<option value="marsabit">Marsabit</option>
+							<option value="sololo">Sololo</option>
+							<option value="moyale">Moyale</option>
+						</select>
+					</div>
+			</div>
+			<div class="form-group">
+				<label for="destination" class="col-lg-4 control-label" >To:</label>
+				<div class="col-lg-8">
+					<select class="form-control" id="to" name="to">
+						<option value="isiolo">Isiolo</option>
+						<option value="nairobi">Nairobi</option>
+						<option value="marsabit">Marsabit</option>
+						<option value="sololo">Sololo</option>
+						<option value="moyale">Moyale</option>
+					</select>
+				</div>
+			</div>
+			<div class="form-group">
+				<label for="departure-date" class="col-lg-6 control-label" >Date of Departure:</label>
+				<div class="col-lg-6">
+				<input type="text" class="form-control" id="departure-date" name="departure-date" placeholder="Date of departure"/>
+				</div>
+			</div>
+			<div class="form-group col-lg-12">
+				<div class="col-lg-6">
+			</div>
 
-<div class="container">
-<div class="container" >
-			<div class="container">
-			<div class="col-lg-6"></div>
-						<form class="form-horizontal col-lg-6" id="booking_details" action="seatsPage.php" method="POST" onsubmit="return validateForm()">
-							<div class="form-group">
-								<label for="source" class="col-lg-4 control-label" >From:</label>
-								<div class="col-lg-8">
-									<select class="form-control" id="from" name="from">
-											<option value="nairobi">Nairobi</option>
-											<option value="isiolo">Isiolo</option>
-											<option value="marsabit">Marsabit</option>
-											<option value="sololo">Sololo</option>
-											<option value="moyale">Moyale</option>
-									</select>
-								</div>
-							</div>
-							<div class="form-group">
-								<label for="destination" class="col-lg-4 control-label" >To:</label>
-								<div class="col-lg-8">
-								<select class="form-control" id="to" name="to">
-										<option value="isiolo">Isiolo</option>
-										<option value="nairobi">Nairobi</option>
-										<option value="marsabit">Marsabit</option>
-										<option value="sololo">Sololo</option>
-										<option value="moyale">Moyale</option>
-								</select>
-								</div>
-							</div>
-							 <div class="form-group">
-								<label for="departure-date" class="col-lg-6 control-label" >Date of Departure:</label>
-									<div class="col-lg-6">
-										<input type="text" class="form-control" id="departure-date" name="departure-date" placeholder="Date of departure"/>
-									</div>
-							</div>
-							<div class="form-group col-lg-12">
-										<div class="col-lg-6">
-										</div>
-
-										<div class="row">
-												<div class="radio col-lg-3">
-													<label>
-														<input type="radio" name="trip-type" id="one-way" value="1" checked>
-															One way
-														</label>
-												</div>
+			<div class="row">
+				<div class="radio col-lg-3">
+					<label><input type="radio" name="trip-type" id="one-way" value="1" checked>One way</label>
+				</div>
 
 												<div class=" radio col-lg-3 pull-right ">
 													<label>
@@ -164,10 +156,10 @@
 											</div>
 								</div>			
 							</form>
-							</div>
-						</div>
+							
+						
 
-					</div>
+					
 				</div>
 			</div>
 		</div>
@@ -175,58 +167,49 @@
 
 <!--page footer-->
 <div class="container">
-<div class="navbar navbar-default navbar-static-bottom">
-	<div class="container">
-		<p class="navbar-text"> Copyright@ 2017 by Zain Services. All Rights Reserved.</p>
+  <div class="row">
+    <div class="col-xs-12">
+	  <div class="well">
+		<footer class="text-center"> Copyright@ 2017 by Zain Services. All Rights Reserved.</footer>
+	  </div>
 	</div>
-</div>
+  </div>
 </div>
 
 <!--Contact us modal with a fill-in form-->
 
-<div class= "modal fade" id="contact" role="dialog" >
+ <div class= "modal fade" id="contact" role="dialog" >
 	<div class="modal-dialog">
 		<div class="modal-content">
-
-
-		<form class="form-horizontal" id="contactUs" method="post" action="contactMoyaleRaha.php">
+		  <form class="form-horizontal" id="contactUs" method="post" action="contactMoyaleRaha.php">
 			<div class="modal-header">
-				<h4>Contact Moyale Raha</h4>
-
+				<h4>Contact Zayn Services</h4>
 			</div>
 			<div class="modal-body">
 				<div class="form-group">
-				
 					<label for="contact-name" class="col-lg-2 control-label" >Name:</label>
 						<div class="col-lg-10">
 							<input type="text" class="form-control" name="name" id="contact-name" placeholder="Full Name">
 						</div>
 				</div>
-
 				<div class="form-group">
-				
 					<label for="contact-email" class="col-lg-2 control-label" >Email:</label>
-						<div class="col-lg-10">
-							<input type="email" class="form-control" id="contact-email" name="email" placeholder="you@example.com">
-						</div>
+					<div class="col-lg-10">
+					  <input type="email" class="form-control" id="contact-email" name="email" placeholder="you@example.com">
+					</div>
 				</div>
-
 				<div class="form-group">
-				
 					<label for="contact-msg" class="col-lg-2 control-label" >Message:</label>
 						<div class="col-lg-10">
 							<textarea class="form-control" rows="8" name="message"></textarea>
 						</div>
 				</div>
-
 			</div>
-
 	            <div class="modal-footer" id="modalfooter">
 					<a class="btn btn-default" data-dismiss="modal"> Cancel</a>
 					<button class="btn btn-primary" type="submit" name="btnsubmit">Send</button>
 				</div>
-
-			</form>
+		  </form>
 			
 
 		</div>
@@ -235,10 +218,12 @@
 
 
 
+
+
 <script src="js/jquery.js"></script>
 <script src="js/code.js"></script>
 <script src="js/jquery-ui/jquery-ui.min.js"></script>
-<script src="js/bootstrap.js"></script>
+
 
 
 </body>
